54d0c806349a723ecf913085f38425365f10519a
Igor Nedopolz <igor.nedopolz@gmail.com>
2024-10-16 00:11:54 +0200

    toast fix

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './Contact.css';
 
 const Contact = () => {
