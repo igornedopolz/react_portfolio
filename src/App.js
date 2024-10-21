919cb29beb8cd5f039b72f123889b655040c0e9a
Igor Nedopolz <igor.nedopolz@gmail.com>
2024-10-21 18:09:04 +0200

    theme switcher to png

--- src/App.js
+++ src/App.js
@@ -68,10 +68,9 @@ function App() {
       <Header scrollPosition={scrollPosition}>
         <button onClick={toggleTheme} className="theme-switcher" aria-label="Toggle Theme">
         {theme === 'dark' ? (
-          <svg stroke="white" fill="white" stroke-width="1" viewBox="0 0 352 512" height="50%" width="50%" xmlns="http://www.w3.org/2000/svg"><path d="M96.06 454.35c.01 6.29 1.87 12.45 5.36 17.69l17.09 25.69a31.99 31.99 0 0 0 26.64 14.28h61.71a31.99 31.99 0 0 0 26.64-14.28l17.09-25.69a31.989 31.989 0 0 0 5.36-17.69l.04-38.35H96.01l.05 38.35zM0 176c0 44.37 16.45 84.85 43.56 115.78 16.52 18.85 42.36 58.23 52.21 91.45.04.26.07.52.11.78h160.24c.04-.26.07-.51.11-.78 9.85-33.22 35.69-72.6 52.21-91.45C335.55 260.85 352 220.37 352 176 352 78.61 272.91-.3 175.45 0 73.44.31 0 82.97 0 176zm176-80c-44.11 0-80 35.89-80 80 0 8.84-7.16 16-16 16s-16-7.16-16-16c0-61.76 50.24-112 112-112 8.84 0 16 7.16 16 16s-7.16 16-16 16z"></path></svg>
+          <img style={{height: "25", width: "25px" }} src="/lightbulb.png" alt="Your Profile" />
         ) : (
-          <svg stroke="white" fill="white" stroke-width="1" viewBox="0 0 512 512" height="50%" width="50%" xmlns="http://www.w3.org/2000/svg"><path d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z"></path></svg>
-        )}
+          <img style={{height: "20", width: "20px" }} src="/moon.png" alt="Your Profile" />)}
         </button>
       </Header>
       <Hero/>
